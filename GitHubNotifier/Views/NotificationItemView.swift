306e00012cd2c089d250b867ecbf5b265bef7ff5
Nikita Pozdeev <octoberallover.nn@gmail.com>
2021-12-10 01:37:22 +0400

    add notification item view

---
+++ GitHubNotifier/Views/NotificationItemView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationItemView.swift
+//  GitHubNotifier
+//
+//  Created by Nikita Pozdeev on 12/11/21.
+//
+
+import SwiftUI
+
+struct NotificationItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NotificationItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationItemView()
+    }
+}
