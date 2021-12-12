ea4306357dedfd5bb1c1b1ef9210c1850026be9f
Nikita Pozdeev <octoberallover.nn@gmail.com>
2021-12-12 23:37:36 +0400

    add quit app button, button tooltips

--- GitHubNotifier/Views/Notifications/NotifactionsHeaderView.swift
+++ GitHubNotifier/Views/Notifications/NotifactionsHeaderView.swift
@@ -22,12 +22,23 @@ struct NotifactionsHeaderView: View {
                 Image(systemName: "gearshape.fill")
                     .foregroundColor(.primary)
             })
+                .help("Settings")
                 .buttonStyle(PlainButtonStyle())
             
             Button(action: {}, label: {
                 Image(systemName: "info.circle.fill")
                     .foregroundColor(.primary)
             })
+                .help("User info")
+                .buttonStyle(PlainButtonStyle())
+            
+            Button(action: {
+                NSApplication.shared.terminate(self)
+            }, label: {
+                Image(systemName: "xmark.circle.fill")
+                    .foregroundColor(.primary)
+            })
+                .help("Close application")
                 .buttonStyle(PlainButtonStyle())
         }
         .padding([.horizontal, .vertical])
